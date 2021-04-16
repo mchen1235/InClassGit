88e8f2282d46461ad2c6bdc7dba89d9841eec7a9
mchen1235 <michaelschen123@gmail.com>
2021-04-15 17:27:25 -0700

    Generates input number of random characters

--- password.py
+++ password.py
@@ -1,9 +1,11 @@
+import random
+
 def password():
 	password = ""
 	length = int(input("How long do you want your password to be: "))
 
 	for i in range(length):
-		password += ""
+		password += chr(random.randint(48, 122))
 
 	print(password)
 
